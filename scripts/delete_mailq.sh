25c89c0725c20ee0f40002c75e1237d9e211c5be
ikushin <you@example.com>
2016-01-27 03:16:05 +0900

    delete_mailq.sh 更新

--- scripts/delete_mailq.sh
+++ scripts/delete_mailq.sh
@@ -1,31 +1,31 @@
-#!/bin/bash -x delete_mailq.sh
+#!/bin/bash -x delete_mailq.sh 3
 #!/bin/bash
 #
 # 概要:
-#   閾値を超えた場合、mailq 内の from = MAILER-DAEMON かつ 指定した TO 宛のメールを削除する。
+#   閾値を超えた場合、mailq 内の任意の FROM/TO 宛のメールを削除する。
 #
 
 # 多重起動チェック
-#if [ $$ -ne $(pgrep -fo "$0") ]; then
-#    logger -t "$0" "error: Cannot run multiple instance"
-#    exit 1
-#fi
+if [ $$ -ne $(pgrep -fo "$0") ]; then
+    logger -t "$0" "error: Cannot run multiple instance."
+    #exit 1
+fi
 
 # 引数チェック
 if [[ $# > 0 ]]; then
     if ! egrep -q '^[0-9]+$' <<<$1; then
-        logger -t "$0" "error: '$1' is not numerical value"
+        logger -t "$0" "error: '$1' is not numerical value."
         exit 1
     fi
     arg=$1
 fi
 
-# この閾値以上で mailq を削除する
+# この閾値以上で mailq を削除する。初期値は0。
 threshold=${arg:=0}
 
-# 削除対象の TO を正規表現で指定する
+# 削除対象の FROM/TO を正規表現で指定する
 from="MAILER-DAEMON"
-to="@example.com$"
+to="^.*@example.com$"
 
 # mailq の数
 nr_mailq=$(mailq 2>/dev/null | egrep -c "^[[:alnum:]]{11}")
@@ -33,24 +33,24 @@ nr_mailq=$(mailq 2>/dev/null | egrep -c "^[[:alnum:]]{11}")
 # nr_mailq が数値であるかチェック
 if ! egrep -q '^[0-9]+$' <<<$nr_mailq
 then
-    logger -t "$0" "error: '$nr_mailq' is not numerical value"
+    logger -t "$0" "error: '$nr_mailq' is not numerical value."
     exit 1
 fi
 
 # 閾値を下回っていたらスクリプト終了
-[[ $nr_mailq < $threshold ]] && exit 1
+[[ $nr_mailq -le $threshold ]] && exit 0
 
-# main 処理
+# mailq の削除
 logger -t "$0" "start"
 output=$( mailq | tail -n +2 | \
              /bin/grep -v '^ *(' | \
-             awk "BEGIN { RS = \"\" } { if( \$7 == $from && \$8 ~ $to && \$9 == \"\") print $1 }" | \
+             awk "BEGIN { RS = \"\" } { if( \$7 ~ \"$from\" && \$8 ~ \"$to\" && \$9 == \"\") print \$1 }" | \
              tr -d '*!' | \
-             sudo postsuper -d - 2>&1 ); rc=$?
+             postsuper -d - 2>&1 ); rc=$?
 
 # 終了メッセージ生成
 if egrep "Deleted" <<<$output; then
-    return_message=$( egrep -o 'postsuper: Deleted:.*messages' <<<$output )
+    return_message=$( egrep -o 'postsuper: Deleted:.*messages?' <<<$output )
 else
     return_message=$output
 fi
