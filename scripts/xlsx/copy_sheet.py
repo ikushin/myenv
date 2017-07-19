8c0f01782c488a03ec8e47e7cd92f6ae1f28b468
ikushin <you@example.com>
2017-07-19 13:44:25 +0900

    Update

---
+++ scripts/xlsx/copy_sheet.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3 a.py example_20110708a.xlsx
+#
+import sys
+import re
+import openpyxl
+import datetime
+
+# コピー先シート名
+nss = [
+    "aaa",
+    "bbb",
+    "ccc",
+]
+
+# ブック
+f=sys.argv[1]
+
+# コピー元シート名
+s='Sheet1'
+
+# 新規ブック名
+_d = datetime.datetime.today()
+d = _d.strftime("%Y%m%d")
+nf = re.sub(r'_\d{8}[a-z]', '_' + d + 'x' , f)
+
+# ブック・シートopen
+wb = openpyxl.load_workbook(f)
+ws = wb.get_sheet_by_name(s)
+
+# シートコピー
+for ns in nss :
+    dist = wb.copy_worksheet(ws)
+    dist.title = ns
+
+# save
+wb.save(nf)
