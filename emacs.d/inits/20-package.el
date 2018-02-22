0d935b7e88b457b633c7f86f3691d11313121741
ikushin <you@example.com>
2018-02-22 11:44:39 +0900

    Update

--- emacs.d/inits/20-package.el
+++ emacs.d/inits/20-package.el
@@ -1,29 +1,29 @@
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
-(package-initialize)
+;; (require 'package)
+;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+;; (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+;; (package-initialize)
 
-(require 'cl)
+;; (require 'cl)
 
-(defvar installing-package-list
-  '(
-    ;; ここに使っているパッケージを書く。
-    minibuffer-complete-cycle
-    redo+
-    point-undo
-    browse-kill-ring
-    recentf-ext
-    yaml-mode
-    bind-key
-    smart-tab
-    auto-shell-command
-    elscreen
-    ))
+;; (defvar installing-package-list
+;;   '(
+;;     ;; ここに使っているパッケージを書く。
+;;     minibuffer-complete-cycle
+;;     redo+
+;;     point-undo
+;;     browse-kill-ring
+;;     recentf-ext
+;;     yaml-mode
+;;     bind-key
+;;     smart-tab
+;;     auto-shell-command
+;;     elscreen
+;;     ))
 
-(let ((not-installed (loop for x in installing-package-list
-                            when (not (package-installed-p x))
-                            collect x)))
-  (when not-installed
-    (package-refresh-contents)
-    (dolist (pkg not-installed)
-        (package-install pkg))))
+;; (let ((not-installed (loop for x in installing-package-list
+;;                             when (not (package-installed-p x))
+;;                             collect x)))
+;;   (when not-installed
+;;     (package-refresh-contents)
+;;     (dolist (pkg not-installed)
+;;         (package-install pkg))))
