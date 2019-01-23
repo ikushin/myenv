5f6b6529c3ed88bbc9bddf4822daeab7992e9575
ikushin <you@example.com>
2019-01-24 08:44:21 +0900

    Update

---
+++ scripts/ping_and_snmp.sh
@@ -0,0 +1,32 @@
+#!/bin/bash ./precheck_add_cisco.sh *.tsv
+# set -e
+# set -u
+# set -v
+# set -x
+LANG=C
+
+file=$1
+[[ -f $file ]] || exit 1
+
+list=$(grep -Pv '^#' $file)
+
+while read -r ip node
+do
+
+    # pingチェック
+    ping -c1 -W1 $ip &>/dev/null; rc=$?
+    if [[ $rc -ne 0 ]]; then
+        echo "$node: PING -> NG" | /bin/grep --color '.*'
+        continue
+    fi
+    echo "$node: PING -> OK"
+
+    # snmpチェック
+    snmpwalk -t 0.3 -v2c -c sol-watch $ip sysDescr &>/dev/null; rc=$?
+    if [[ $rc -ne 0 ]]; then
+        echo "$node: SNMP -> NG" | /bin/grep --color '.*'
+        continue
+    fi
+    echo "$node: SNMP -> OK"
+
+done <<<"$list"
